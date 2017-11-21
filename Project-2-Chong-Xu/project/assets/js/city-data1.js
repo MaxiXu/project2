51fe3637a2bb01751b06549383d144631ac30c03
MaxiXu <jiajia68097285@gmail.com>
2017-11-20 20:05:01 -0500

    project2
    project2

---
+++ Project-2-Chong-Xu/project/assets/js/city-data1.js
@@ -0,0 +1,107 @@
+var cityData = {
+	f1: {
+    city: "Tokyo",
+		area: '845 mi²',
+		localtime: 'Wednesday 12:48 PM',
+		population:'9.273 million (2015)',
+		island:"Honshu"
+	},
+	s11: {
+		city: "Kyoto",
+		area: '319.6 mi²',
+		localtime: 'Wednesday 12:54 PM',
+		population:'1.475 million (2015)',
+		island:"Honshu"
+	},
+	s12: {
+		city: "Osaka",
+		area: '86.1 mi²',
+		localtime: 'Wednesday 12:57 PM',
+		population:'2.691 million (2015)',
+		island:"Honshu"
+		},
+	s13: {
+		city: "Nagoya",
+		area: '126 mi²',
+		localtime: 'Wednesday 1:00 PM',
+		population:'2.296 million (2015)',
+		island:"Honshu"
+	},
+	s14: {
+		city: "Kobe",
+		area: '213.2 mi²',
+		localtime: 'Wednesday 1:06 PM',
+		population:'1.537 million (2015)',
+		island:"Kyushu"
+	},
+	s15: {
+		city: "Yokohama",
+		area: '168.9 mi²',
+		localtime: 'Wednesday 1:10 PM',
+		population:'3.725 million (2015)',
+		island:"Honshu"
+	},
+	s16: {
+		city: "Fukuoka",
+		area: '131.3 mi²',
+		localtime:'Wednesday 1:12 PM',
+		population:'1.539 million (2015)',
+		island:"Honshu"
+	},
+	s17: {
+		city: "Sapporo",
+	area: '433 mi²',
+	localtime:'Wednesday 1:17 PM',
+	population:'1.952 million (2015)',
+	island:"Hokkaido"
+},
+	s21: {
+		city: "Nara",
+ area: '106.9 mi²',
+ localtime:'Wednesday 1:21 PM',
+ population:'360,310 (2015)',
+ island:"Honshu"
+	},
+	s22: {
+		city: "Hiroshima",
+	area: '350 mi²',
+	localtime:'Wednesday 1:24 PM',
+	population:'1.194 million (2015)',
+	island:"Honshu"
+	},
+	s23: {
+		city: "Kitakyushu",
+	area: '188 mi²',
+	localtime:'Wednesday 1:27 PM',
+	population:'961.286 million (2015)',
+	island:"Honshu"
+	},
+	s24: {
+		city: "Sendai",
+	area: '303.4 mi²',
+	localtime:'Wednesday 1:28 PM',
+	population:'1.082 million (2015)',
+	island:"Honshu"
+	},
+	s25: {
+		city: "Nagasaki",
+	area: '256.9 mi²',
+	localtime:'Wednesday 1:29 PM',
+	population:'429.508 million (2015)',
+	island:"Kyushu"
+	},
+	s26: {
+		city: "Kanazawa",
+		area: '180.6 mi²',
+		localtime:'Wednesday 1:30 PM',
+		population:'465.699 million (2015)',
+		island:"Honshu"
+	},
+	s27: {
+	city: "Shinjuku",
+	area: '7.039 mi²',
+	localtime:'Wednesday 1:27 PM',
+	population:'337.556 million (2015)',
+	island:"Honshu"
+	},
+};
